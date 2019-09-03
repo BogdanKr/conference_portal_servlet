b96e17e14feccf29da0d53510768b88480b944b5
Bogdan <krabog@gmail.com>
2019-09-03 15:03:39 +0300

    start project, filter done

---
+++ src/main/java/ua/krasun/conference_portal_servlet/FirstServlet.java
@@ -0,0 +1,37 @@
+package ua.krasun.conference_portal_servlet;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class FirstServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+
+//    @Override
+//    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        processRequest(req, resp);
+//
+//    }
+
+    private void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String path = request.getRequestURI();
+        response.getWriter().print("Hello! from servlet : " + path + " ");
+        path = path.replaceAll(".*/app", "");
+        response.getWriter().print("<br>");
+//        response.getWriter().print("replaceAll(\".*/app\", \"\") path: " + path);
+        response.sendRedirect("https://google.com");
+
+//        request.getRequestDispatcher("/index.jsp").forward(request, response);
+    }
+}
+
+
