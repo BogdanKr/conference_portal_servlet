283b255e843330d59ab36c997db29cd227122d62
Bogdan <krabog@gmail.com>
2019-09-18 14:28:55 +0300

    add myRegistration on conf

--- src/main/java/ua/krasun/conference_portal_servlet/controller/command/AdminRole.java
+++ src/main/java/ua/krasun/conference_portal_servlet/controller/command/AdminRole.java
@@ -1,10 +1,16 @@
 package ua.krasun.conference_portal_servlet.controller.command;
 
+import ua.krasun.conference_portal_servlet.model.entity.User;
+import ua.krasun.conference_portal_servlet.model.service.ConferenceService;
+
 import javax.servlet.http.HttpServletRequest;
 
 public class AdminRole implements Command {
     @Override
     public String execute(HttpServletRequest request) {
+        ConferenceService conferenceService = new ConferenceService();
+        request.getSession().setAttribute("conferenceList",
+                conferenceService.findAllConference(((User) request.getSession().getAttribute("user")).getId()));
             return "/WEB-INF/admin/adminbasic.jsp";
     }
 }
